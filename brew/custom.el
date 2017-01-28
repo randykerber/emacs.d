e4a63c733b36a163f3ffebeb3746d20a829314de
Randy Kerber <randy@randykerber.com>
2017-01-28 15:48:47 -0500

    Save the custom.el file for brew.

---
+++ brew/custom.el
@@ -0,0 +1,40 @@
+;; (message "Loading: ~/gh/randykerber/emacs.d/custom.el")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-file "~/gh/randykerber/emacs.d/brew/custom.el")
+ '(dired-dwim-target t)
+ '(fill-column 96)
+ '(font-lock-global-modes t)
+ '(frame-resize-pixelwise t)
+ '(indent-tabs-mode nil)
+ '(mouse-drag-copy-region t)
+ '(next-line-add-newlines t)
+ '(ns-auto-hide-menu-bar t)
+ '(ns-command-modifier (quote alt))
+ '(package-archive-priorities (quote (("melpa-stable" . 10))))
+ '(package-archives
+   (quote
+    (("melpa-stable" . "https://stable.melpa.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")
+     ("gnu" . "http://elpa.gnu.org/packages/"))))
+ '(package-selected-packages (quote (markdown-mode csv yaml-mode)))
+ '(require-final-newline t)
+ '(safe-local-variable-values (quote ((no-byte-compile . t))))
+ '(scroll-conservatively 99)
+ '(scroll-step 1)
+ '(show-paren-mode t)
+ '(tab-width 4)
+ '(tool-bar-mode nil)
+ '(track-eol t)
+ '(user-mail-address "randy@randykerber.com")
+ '(window-resize-pixelwise t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
