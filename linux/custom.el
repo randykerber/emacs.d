fada0efb0d0f31882dbf3459babfbf9eb570ebd4
Randy Kerber <randy@randykerber.com>
2017-11-26 22:27:29 -0500

    custom.el files for 'brew' and 'linux'.

--- linux/custom.el
+++ linux/custom.el
@@ -5,7 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-file "~/gh/randykerber/emacs.d/brew/custom.el")
+ '(custom-file "~/gh/randykerber/emacs.d/linux/custom.el")
  '(dired-dwim-target t)
  '(fill-column 96)
  '(font-lock-global-modes t)
