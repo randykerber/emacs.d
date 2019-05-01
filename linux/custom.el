1c7598101128a1db501c863020046830f66e6746
Randy Kerber <randy@randykerber.com>
2019-05-01 19:44:22 -0400

    Remove a bunch of entries from custom.el for brew and linux.

--- linux/custom.el
+++ linux/custom.el
@@ -5,37 +5,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-file "~/gh/randykerber/emacs.d/linux/custom.el")
  '(dired-dwim-target t)
- '(fill-column 96)
- '(font-lock-global-modes t)
- '(frame-resize-pixelwise t)
- '(indent-tabs-mode nil)
  '(mouse-drag-copy-region t)
- '(next-line-add-newlines t)
- '(ns-auto-hide-menu-bar t)
- '(ns-command-modifier (quote alt))
- '(package-archive-priorities (quote (("melpa-stable" . 20) ("elpa" . 10) ("melpa" . 0))))
- '(package-archives
-   (quote
-    (("melpa-stable" . "http://stable.melpa.org/packages/")
-     ("melpa" . "http://melpa.org/packages/")
-     ("gnu" . "http://elpa.gnu.org/packages/"))))
  '(package-selected-packages
    (quote
-    (exec-path-from-shell smartparens dired+ whole-line-or-region bash-completion osx-location s f which-key ivy)))
- '(require-final-newline t)
+    (exec-path-from-shell smartparens whole-line-or-region bash-completion which-key)))
  '(safe-local-variable-values (quote ((no-byte-compile t) (no-byte-compile . t))))
  '(scroll-conservatively 99)
  '(scroll-step 1)
- '(show-paren-mode t)
- '(tab-width 4)
- '(tool-bar-mode nil)
- '(track-eol t)
- '(use-package-always-defer t)
- '(use-package-always-ensure t)
- '(use-package-verbose t)
- '(user-mail-address "randy@randykerber.com")
+ '(frame-resize-pixelwise t)
  '(window-resize-pixelwise t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
