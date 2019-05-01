3719edfcb1e94e0f69ea8663e66bffaf5a6d5e1b
Randy Kerber <randy@randykerber.com>
2019-05-01 18:26:13 -0400

    All package usage now in packages.el using 'use-package'.

--- common/packages.el
+++ common/packages.el
@@ -9,11 +9,47 @@
 ;;(setq use-package-always-defer t)
 
 
-(use-package f)
-(use-package s)
-(use-package dash
-  ;; :mode "\\.el$" "\\.lisp$"
-)
+(use-package diminish)
+(use-package bind-key)
+
+;;; [use-package]( https://github.com/jwiegley/use-package )
+
+
+(use-package better-defaults)
+
+
+(use-package text-mode
+  :ensure nil
+  :defer t
+  ;; :preface (provide 'text-mode)
+  ;; :mode (("\\.txt\\'" . text-mode)
+  ;;        ("\\.log\\'" . text-mode))
+  :mode "\\.txt$"
+  ;; :hook (turn-on-word-wrap
+  ;;        turn-on-auto-fill)
+  ;; :config
+  ;; (turn-on-word-wrap)
+  ;; (turn-on-auto-fill)
+  ;; :init
+  ;; (remove-hook 'text-mode-hook 'smart-spacing-mode)
+  ;; (remove-hook 'text-mode-hook 'auto-detect-wrap)
+  ;; (add-hook 'text-mode-hook 'turn-on-auto-fill) ; wrap long lines in text mode
+  ;; (add-hook 'text-mode-hook 'turn-on-word-wrap)
+  )
+
+(use-package f :disabled)
+(use-package s :disabled)
+;;(use-package dash
+  ;;:demand t
+  ;;:mode "\\.el$" "\\.lisp$"
+;;)
+
+(use-package ido
+  ;; :disabled
+  :config
+  (setq ido-show-dot-for-dired t)
+  )
+
 (use-package company)
 
 (use-package sparql-mode
@@ -34,31 +70,22 @@
 ;; sqlite
 
 (use-package pkg-info)
-(use-package magit)
+;; (use-package magit)
 
 
 (use-package markdown-mode
   :ensure t
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.markdown\\'" . markdown-mode)
          ("\\.md\\'" . gfm-mode)
-         )
+         ("\\.gfm\\'" . gfm-mode)
+         ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
+
 
 ;; (use-package gfm-mode
 ;;              :mode "\\.md$" "\\.gfm$")
 ;; (use-package markdown-mode)
-
-(use-package text-mode
-             :ensure nil
-             :mode "\\.txt$" "\\.log$"
-             :init
-             (remove-hook 'text-mode-hook 'smart-spacing-mode)
-             (remove-hook 'text-mode-hook 'auto-detect-wrap)
-             (add-hook 'text-mode-hook 'turn-on-auto-fill) ; wrap long lines in text mode
-             (add-hook 'text-mode-hook 'turn-on-word-wrap)
-             )
 
 (use-package whitespace-cleanup-mode
              :config
@@ -77,39 +104,45 @@
 ;; (use-package smex)
 
 
-(use-package osx-location
-             :defer nil)
-(use-package magit-popup)
+(use-package osx-location)
+
+;; (use-package magit-popup)
+
 ;; (use-package async)
+
 (use-package bash-completion
-             :mode "\\.sh$")
+             :mode ("\\.sh$" "\\.bash$")
+             )
+
+(use-package eshell)
+
 ;; (use-package epl)
-;; (use-package eshell)
 ;; (use-package flycheck)
 ;; (use-package request)
 ;; (use-package slime)
 ;; (use-package tablist)
 ;; (use-package yasnippet)
-             
+
 ;; (use-package flycheck-scala-sbt)
 
 
-(use-package ruby-mode
-             :mode "\\.rb$"
-             :interpreter "ruby")
+;;(use-package ruby-mode
+;;             :mode "\\.rb$"
+;;             :interpreter "ruby")
 ;; (use-package scala-mode)
 ;; (use-package sbt-mode)
 ;; (use-package ensime)
+
 (use-package yaml-mode
-             :mode "\\.yml$"
+             :mode ("\\.yml$" "\\.yaml$")
              :init
              (add-hook 'yaml-mode-hook
                        '(lambda ()
                           (define-key yaml-mode-map "\C-m" 'newline-and-indent))))
 ;; (use-package org)
 ;; (use-package helm)
-;; (use-package ivy
-;;              :ensure t)
+
+(use-package ivy)
 
 (use-package tramp
              :config
@@ -126,47 +159,61 @@
 ;; (use-package docker)
 ;; (use-package dockerfile-mode)
 
-(use-package which-key
-             :ensure t)
+(use-package which-key)
+
 ;; (use-package discover)
 ;; (use-package discover-my-major)
+
 (use-package whole-line-or-region)
-;; (use-package )
 
 (use-package json-mode
-  :ensure t
   :mode (("\\.json\\'" . json-mode)
          ("\\.tmpl\\'" . json-mode)
          ("\\.eslintrc\\'" . json-mode))
   :config (setq-default js-indent-level 2))
 
 (use-package json-reformat
-  :ensure t
   :after json-mode
   :bind (("C-c r" . json-reformat-region)))
 
+(use-package python
+  :mode ("\\.py\\'" . python-mode)
+  )
+(use-package elpy
+  :after python-mode
+  :init (elpy-enable))
+
+(use-package init-scala
+  :disabled
+  :mode (("\\.scala" . scala-mode))
+  :load-path "lisp/")
+
+(use-package ess-site
+  :disabled
+  :commands R)
+
 (use-package dired+
-  :ensure t
-  :defer t
+  :disabled
   :config
-  (setq diredp-hide-details-initially-flag nil)
-  (set-face-foreground 'diredp-file-name nil)
+  ;;(setq diredp-hide-details-initially-flag nil)
+  ;;(set-face-foreground 'diredp-file-name nil)
   ;; Keep dired buffers updated when the file system changes.
-  (setq global-auto-revert-non-file-buffers t)
-  (setq auto-revert-verbose nil))
+  ;;(setq global-auto-revert-non-file-buffers t)
+  ;;(setq auto-revert-verbose nil)
+  )
 
 (use-package smartparens
-  :ensure t
+  :demand t
   :init (smartparens-global-mode 1)
-  :diminish smartparens-mode)
+  :diminish smartparens-mode
+  )
 
 (use-package exec-path-from-shell
-  :ensure t
   :if (memq window-system '(mac ns))
   :config
   (setq exec-path-from-shell-arguments '("-l"))
   (exec-path-from-shell-initialize)
-  (exec-path-from-shell-copy-env "PATH")
+  ;; (exec-path-from-shell-copy-env "PATH")
   )
 
 ;; (use-package csv-mode
@@ -177,7 +224,26 @@
 ;;              :mode "\\.csv$" "\\.tsv$"
 ;;              :config
 ;;              (setq csv-separators '("," ";" "|" " " "	")))
+
 (use-package csv
-             :mode "\\.csv$" "\\.tsv$"
+             :mode ("\\.csv$" "\\.tsv$")
              :config
-             (setq csv-separators '("," ";" "|" " " "	")))
+             (setq csv-separators '("," ";" "|" " " "	"))
+             )
+
+;;(use-package smart-mode-line
+;;  :ensure t
+;;  )
+;;
+;; (sml/setup)
+
+(use-package key-chord
+  :disabled
+  :config
+  (key-chord-mode 1)
+  (key-chord-define-global "--" "_")
+  )
+
+;; http://www.funtoo.org/Keychain
+;;(use-package keychain-environment
+;;  :init (keychain-refresh-environment))
