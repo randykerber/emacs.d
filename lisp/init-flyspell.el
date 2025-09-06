e0c234521541680d0b68ef2678fd63884b26ae89
Randy Kerber <randy@randykerber.com>
2025-09-06 17:45:29 -0600

    Fix flyspell init

--- lisp/init-flyspell.el
+++ lisp/init-flyspell.el
@@ -1,8 +1,15 @@
 ;;----------------------------------------------------------------------------
 ;; Add spell-checking in comments for all programming language modes
 ;;----------------------------------------------------------------------------
+
+;; Conditional flyspell activation that skips csv-mode
+(defun maybe-enable-flyspell-prog-mode ()
+  "Enable flyspell-prog-mode unless in csv-mode."
+  (unless (eq major-mode 'csv-mode)
+    (flyspell-prog-mode)))
+
 (if (fboundp 'prog-mode)
-    (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+    (add-hook 'prog-mode-hook 'maybe-enable-flyspell-prog-mode)
   (dolist (hook '(lisp-mode-hook
                   emacs-lisp-mode-hook
                   scheme-mode-hook
@@ -20,7 +27,7 @@
                   perl-mode-hook
                   tcl-mode-hook
                   javascript-mode-hook))
-    (add-hook hook 'flyspell-prog-mode)))
+    (add-hook hook 'maybe-enable-flyspell-prog-mode)))
 
 (after-load 'flyspell
   (add-to-list 'flyspell-prog-text-faces 'nxml-text-face))
